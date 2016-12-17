f5d194faa9bb9b26b297fb86cc6a93d897433373
daedeloth <thijs@catlab.be>
2016-12-17 14:58:17 +0100

    Making everything more php7 accpetable :)

--- php/Modules/Core.php
+++ php/Modules/Core.php
@@ -7,39 +7,40 @@
  */
 
 class Modules_Core
-	extends Modules_Page
+    extends Modules_Page
 {
-	public function getHTML ($template = 'index.phpt')
-	{
-		$url = $this->objCMS->getAction ();
-		$path = CMS_SYSTEM_PATH . 'core/' . $url;
+    public function getHTML ($template = 'index.phpt')
+    {
+        $url = $this->objCMS->getAction ();
+        $path = CMS_SYSTEM_PATH . 'core/' . $url;
 
-		if (file_exists ($path))
-		{
-			$extension = end (explode ('.', $path));
-			switch (strtolower ($extension))
-			{
-				case 'js':
-					header ('Content-type: application/json');
-				break;
+        if (file_exists ($path))
+        {
+            $parts = explode ('.', $path);
+            $extension = end ($parts);
+            switch (strtolower ($extension))
+            {
+                case 'js':
+                    header ('Content-type: application/json');
+                    break;
 
-				case 'gif':
-				case 'png':
-				case 'jpg':
-				case 'jpeg':
-					header ('Content-type: image/' . $extension);
-				break;
+                case 'gif':
+                case 'png':
+                case 'jpg':
+                case 'jpeg':
+                    header ('Content-type: image/' . $extension);
+                    break;
 
-				case 'css':
-					header ('Content-type: text/css');
-				break;
-			}
+                case 'css':
+                    header ('Content-type: text/css');
+                    break;
+            }
 
-			return file_get_contents ($path);
-		}
-		else
-		{
-			return 'File not found: ' . $path;
-		}
-	}
+            return file_get_contents ($path);
+        }
+        else
+        {
+            return 'File not found: ' . $path;
+        }
+    }
 } 
